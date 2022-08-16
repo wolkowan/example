6645376146a5b1604cdc026eb9b14869cebe6e44
Natalia <wolkowan@yandex.ru>
2022-08-16 21:50:37 +0300

    randome example004

--- example004_div/Program.cs
+++ example004_div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
 
