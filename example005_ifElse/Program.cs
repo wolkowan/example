58fe96c58142b9c6809b6dc40f8f188d2027a3c5
Natalia <wolkowan@yandex.ru>
2022-08-16 23:01:31 +0300

    example005

---
+++ example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("Hello, Maria! Glade to see you!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
