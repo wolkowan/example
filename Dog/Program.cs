0584b9093a8697bb759ed9aaa8cf261c356f52fc
Natalia <wolkowan@yandex.ru>
2022-08-17 19:40:59 +0300

    dog

---
+++ Dog/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double firstSpeed = 200, secondSpeed = 300, dogspeed = 700;
+        int count = 0, friend = 2;
+        double distance = 20000, time = 0;
+
+        while (distance > 10)
+        {
+            if (friend == 1)
+            {
+                time = distance / (firstSpeed + dogspeed);
+                friend = 2;
+
+            }
+            else
+            {
+                time = distance / (secondSpeed + dogspeed);
+                friend = 1;
+            }
+            distance = distance - time * (firstSpeed + secondSpeed);
+            count += 1;
+        }
+        Console.Write("Собака пробeжит ");
+        Console.Write(count);
+        Console.Write(" раз");
+    }
+}
